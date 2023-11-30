f44b3c1fce20f580519661ebd65d85895ccd5a57
Javier Rodriguez <xjavierx1995@gmail.com>
2023-11-30 08:03:01 -0400

    prime vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { computed, onMounted } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
-import { useStore } from 'vuex'
+import { useStore } from 'vuex';
+import Button from "primevue/button";
 const store = useStore()
 
 async function hello() {
@@ -23,22 +24,13 @@ onMounted(() => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <Button label="Submit"/>
+  <Button label="Success" severity="success" />
+
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import store from './store';
+import PrimeVue from 'primevue/config';
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(PrimeVue).mount('#app');
